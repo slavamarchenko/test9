4c3412af9fc9ecc957437edc1b1b64d5f409a87b
Slava Marchenko <slava@freshcut.gg>
2024-01-30 14:59:49 +0200

    Added splash

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 
 void main() {
+  WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
+
   runApp(const MyApp());
+
+  Future.delayed(
+    const Duration(seconds: 3),
+    () {
+      FlutterNativeSplash.remove();
+    },
+  );
 }
 
 class MyApp extends StatelessWidget {
